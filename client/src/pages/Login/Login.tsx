646fedaecff87a8485a559da76d730c3b2f82d36
Sarah Smandack <sarah.smandack@gmail.com>
2025-02-24 18:23:09 +0100

    login page style done

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { type Dispatch, type SetStateAction, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import InputField from "../../components/Register/InputField";
+import "./Login.css";
+import logo from "/logo.png";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -7,30 +10,91 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [criteria, setCriteria] = useState({
+    email: "❌ L'email doit être de type address@example.com",
+    passwordLength: "❌ Minimum 12 caractères",
+    passwordLowercase: "❌ Au moins 1 minuscule",
+    passwordUppercase: "❌ Au moins 1 majuscule",
+    passwordNumber: "❌ Au moins 1 chiffre",
+    passwordSpecialChar: "❌ Au moins 1 caractère spécial",
+  });
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const validateEmail = (email: string) => {
+    setCriteria((prev) => ({
+      ...prev,
+      email: /\S+@\S+\.\S+/.test(email)
+        ? "✅  Email valide"
+        : "❌  L'email doit être de type address@example.com",
+    }));
+  };
+
+  const validatePassword = (password: string) => {
+    setCriteria((prev) => ({
+      ...prev,
+      passwordLength:
+        password.length >= 12
+          ? "✅ Minimum 12 caractères"
+          : "❌ Minimum 12 caractères",
+      passwordLowercase: /[a-z]/.test(password)
+        ? "✅ Au moins 1 minuscule"
+        : "❌ Au moins 1 minuscule",
+      passwordUppercase: /[A-Z]/.test(password)
+        ? "✅ Au moins 1 majuscule"
+        : "❌ Au moins 1 majuscule",
+      passwordNumber: /\d/.test(password)
+        ? "✅ Au moins 1 chiffre"
+        : "❌ Au moins 1 chiffre",
+      passwordSpecialChar: /[!@#$%^&*]/.test(password)
+        ? "✅ Au moins 1 caractère spécial"
+        : "❌ Au moins 1 caractère spécial",
+    }));
+  };
+
+  const togglePasswordVisibility = (
+    setState: Dispatch<SetStateAction<boolean>>,
+  ) => {
+    setState((prevState) => !prevState);
+  };
+
   return (
-    <main>
+    <main className="login">
+      <img src={logo} alt="" />
       <h1>Connexion</h1>
       <form>
-        <label htmlFor="email">Email</label>
-        <input
+        <InputField
+          label="Votre email"
           type="email"
-          id="email"
+          name="email"
           value={email}
-          placeholder="Votre email"
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="on"
-          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+            validateEmail(e.target.value);
+          }}
+          criteria={criteria.email}
         />
 
-        <label htmlFor="password">Mot de passe</label>
-        <input
-          type="password"
-          id="password"
+        <InputField
+          label="Votre mot de passe"
+          type={showPassword ? "text" : "password"}
+          name="password"
           value={password}
-          placeholder="Votre mot de passe"
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="on"
-          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+            validatePassword(e.target.value);
+          }}
+          criteria={[
+            criteria.passwordLength,
+            criteria.passwordLowercase,
+            criteria.passwordUppercase,
+            criteria.passwordNumber,
+            criteria.passwordSpecialChar,
+          ]}
+          showPassword={showPassword}
+          togglePasswordVisibility={() =>
+            togglePasswordVisibility(setShowPassword)
+          }
         />
 
         <button type="submit">Se connecter</button>
