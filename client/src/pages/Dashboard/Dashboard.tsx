c39a7cabfbee7c3f8079e95f94d7df97e215d312
Sarah Smandack <sarah.smandack@gmail.com>
2025-03-04 13:42:38 +0100

    delete unused files and export species/users dashboard in seperate files

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,93 +1,15 @@
-import { useEffect, useState } from "react";
-import SpecieDisplay from "../../services/SpecieDisplay";
-import type { Specie, User } from "../../types/types";
 import "./Dashboard.css";
-import AddSpeciesForm from "../../components/Dashboard/Species/AddSpeciesForm";
-import formatDate from "../../services/formatDate";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Dashboard() {
-  const [species, setSpecies] = useState<Specie[]>([]);
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/users`, {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error(error));
-
-    fetch(`${import.meta.env.VITE_API_URL}/api/species`, {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data) => setSpecies(data))
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
     <main className="dashboard">
       <h1>Dashboard</h1>
-      <h2>Espèces :</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Espèce</th>
-            <th>Force de base</th>
-            <th>Vitesse de base</th>
-            <th>Endurance de base</th>
-            <th>Image version bébé</th>
-            <th>Image version adulte</th>
-          </tr>
-        </thead>
-        <tbody>
-          {species.map((specie) => (
-            <tr key={specie.id}>
-              <td>
-                <SpecieDisplay specie={specie.specie} />
-              </td>
-              <td>{specie.base_strength}</td>
-              <td>{specie.base_speed}</td>
-              <td>{specie.base_stamina}</td>
-              <td>
-                <img
-                  src={`${import.meta.env.VITE_API_URL}/${specie.url_baby}`}
-                  alt={`${specie.specie} bébé`}
-                />
-              </td>
-              <td>
-                <img
-                  src={`${import.meta.env.VITE_API_URL}/${specie.url_adult}`}
-                  alt={`${specie.specie} adulte`}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <AddSpeciesForm />
-
-      <h2>Utilisateurs :</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Créé le</th>
-            <th>Administrateur</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.email}</td>
-              <td>{user.created_at ? formatDate(user.created_at) : ""}</td>
-              <td>{user.isAdmin ? "✅" : "❌"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <nav>
+        <Link to="/dashboard/species">Espèces</Link>
+        <Link to="/dashboard/users">Utilisateurs</Link>
+      </nav>
+      <Outlet />
     </main>
   );
 }
